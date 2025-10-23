9f66e3f430a2407e82af12ef40a55bb4b61922b7
Kevin March <kevinmarch@cds.com.py>
2025-10-23 11:45:52 -0300

    SortBy

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -27,7 +27,7 @@ function CabinTable() {
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
-
+  // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins = cabins;
@@ -43,6 +43,17 @@ function CabinTable() {
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) =>
+      (typeof a[field] === "string"
+        ? a[field].localeCompare(b[field])
+        : a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -55,7 +66,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,27 @@
+import Select from "./Select";
+import PropTypes from "prop-types";
+import { useSearchParams } from "react-router-dom";
+
+function SortBy({ options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy") || options[0].value;
+
+  function handleChange(e) {
+    searchParams.set("sortBy", e.target.value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <Select
+      options={options}
+      type="white"
+      onChange={handleChange}
+      value={sortBy}
+    />
+  );
+}
+SortBy.propTypes = {
+  options: PropTypes.array.isRequired,
+};
+
+export default SortBy;
