fa2663d06d0c0affd2b37a1a92c097ca1dde3d7a
Kevin March <kevinmarch@cds.com.py>
2025-10-24 19:21:04 -0300

    Login

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "../ui/Spinner";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  //1. load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  //2. If there is No authenticated user, redirect to login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) {
+        navigate("/login");
+      }
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  //3. while loading, show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  //4. If there IS a user, render the protected route
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
