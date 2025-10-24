6721006be710423036925329422774d6505a98d3
Kevin March <kevinmarch@cds.com.py>
2025-10-24 18:51:40 -0300

    initial login

---
+++ src/services/apiAuth.js
@@ -0,0 +1,14 @@
+import supabase from "./supabase";
+
+export async function login({ email, password }) {
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  console.log("Login successful:", data);
+  return data;
+}
