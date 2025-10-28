656a2d445bf3dc7a2b5f63beb126ee4aae564581
Kevin March <kevinmarch@cds.com.py>
2025-10-28 11:27:00 -0300

    publishable key for supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
-//replace supabasekey for real key and
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://judtomxqebstqfwhowyr.supabase.co";
-const supabaseKey = supabaseKey;
+const supabaseKey = "sb_publishable_UQ6llMWRWFWB56Un1BdwOQ_TeOJWn44";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
